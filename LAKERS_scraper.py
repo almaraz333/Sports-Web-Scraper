6660610d1f989ab116e9eaae7942a28eb4f41afb
almaraz333 <60356596+almaraz333@users.noreply.github.com>
2020-03-03 15:09:03 -0500

    Add files via upload

---
+++ LAKERS_scraper.py
@@ -0,0 +1,78 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import csv
+
+
+website_url = requests.get("https://www.basketball-reference.com/teams/LAL/").text
+soup = BeautifulSoup(website_url,'lxml')
+div = soup.find("div", {"id": "div_LAL"})
+bad_char=['*']
+name_list=[]
+tr=[]
+th=[]
+links=[]
+name_list=[]
+dob_list=[]
+year_list=[]
+for table in div:
+    for tbody in table:
+        tbody = table.find("tbody")
+    try:
+        for tr in tbody:
+            tr = tbody.find_all("tr")
+            tr.append(tr)
+ 
+    except:
+        pass
+for th in tr:
+    try:
+        th = th.find(("th", {"class": "left"}))
+        th.append(th)
+    except:
+        pass
+for a in th:
+    try:
+        a = a.find("a")
+        link = a.attrs['href']
+        if int(link[-9:-5]) > 1964:
+            url = "https://www.basketball-reference.com"+link
+            links.append(url)
+    except:
+        pass
+
+
+for link in links:
+    website_url = requests.get(link).text
+    soup = BeautifulSoup(website_url,'lxml')
+    table = soup.find("table", {"id": "roster"})
+    tbody = table.find("tbody")
+    for tr in tbody:
+        td = tr.find("td")
+        try:
+            dob = tr.find("td", {"data-stat": "birth_date"})
+            dob = dob.text
+            a = td.find("a")
+            name = a.text
+            name_list.append(name)
+            dob_list.append(dob)
+            year_list.append(link[-9:-5])
+        except:
+            pass
+
+    pre_final_list=zip(name_list,dob_list,year_list)
+    final_list=[]
+    for item in pre_final_list:
+        if item not in final_list:
+            final_list.append(item)
+    with open('LAKERS_players.csv', 'w',encoding='utf-8', newline='') as file:
+        writer = csv.writer(file)
+        for item in final_list:
+            writer.writerow(item)
+            
+
+    
+
+
+
+
---
+++ searcher_eams.py
@@ -0,0 +1,81 @@
+import time
+import unidecode
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+import csv
+
+
+
+file = 'Minor league hockey no EAMs.csv'
+hascase=[]
+
+
+browser = webdriver.Firefox()
+browser.get('https://eams.dwc.ca.gov/WebEnhancement/InjuredWorkerFinder')
+
+firstname = browser.find_element_by_id('firstname')
+lastname = browser.find_element_by_id('lastname')
+email = browser.find_element_by_id('email')
+reason = browser.find_element_by_xpath("/html/body/div[1]/div/div/form/table/tbody/tr[10]/td/select/option[3]")
+nextbutton = browser.find_element_by_xpath("/html/body/div[1]/div/div/form/table/tbody/tr[11]/th/input[3]")
+
+firstname.send_keys("Joe")
+lastname.send_keys("Mama")
+email.send_keys("sdgadsgadg@adgagasd.net")
+reason.click()
+nextbutton.click()
+
+dob = browser.find_element_by_id('dob')
+firstname = browser.find_element_by_id('firstname')
+lastname = browser.find_element_by_id('lastname')
+search = browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div/form/p/input[3]")
+
+
+i=0
+with open(file,'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    writer = csv.writer(csvfile)
+    try:
+        for row in reader:
+            try:
+                name = row[0].split()
+                first = name[0][0]
+                last = name[1:]
+                browser.find_element_by_id("firstname").send_keys(Keys.CONTROL + "a")
+                browser.find_element_by_id("firstname").send_keys(Keys.DELETE)
+                browser.find_element_by_id("lastname").send_keys(Keys.CONTROL + "a")
+                browser.find_element_by_id("lastname").send_keys(Keys.DELETE)
+                browser.find_element_by_id("dob").send_keys(Keys.CONTROL + "a")
+                browser.find_element_by_id("dob").send_keys(Keys.DELETE)
+                browser.find_element_by_id('firstname').send_keys(first)
+                browser.find_element_by_id('lastname').send_keys(last)
+                browser.find_element_by_id('dob').send_keys(row[9][:6]+"19"+row[9][6:])
+                browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div/form/p/input[3]").click()
+                try:
+                    browser.find_element_by_xpath("/html/body/div[1]/div/div[3]/div/table")
+                    hascase.append(row[0])
+                except:
+                    pass
+            except:
+                print(row[0]+" 2")
+    except:
+        print(row[0]+" 3")
+            
+
+with open(file,'r') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for row in reader:
+        if row[0] in hascase:
+            print("YES")
+        if row[0] not in hascase:
+            print("NO")
+
+
+                
+
+browser.quit()
+
